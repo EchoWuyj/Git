89977cacd822e858e48a9bd7cf399838144d7ad6
wyj <zgjsnjwyj@163.com>
2022-03-07 13:53:23 +0800

    Git 2022-3-7 13:53:18

---
+++ src/main/java/com/wyj/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.wyj.git;
+
+/**
+ * @Author Wuyj
+ * @DateTime 2022-03-07 13:43
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
